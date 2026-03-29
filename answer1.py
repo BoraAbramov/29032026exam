411c76300ff4fb0fd06db70626f8b1fa582a6a11
BoraAbramov <borya812@gmail.com>
2026-03-29 17:48:37 +0300

    step 1

--- answer1.py
+++ answer1.py
@@ -1,2 +1,39 @@
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+grades = list()
+_grade_in = None
+
+while True:
+    try:
+        _grade_in = int(input("Enter grade: "))
+    except ValueError:
+        print("Invalid input")
+        continue
+    if _grade_in == -999:
+        if len(grades) < 10:
+            print("not enough grades")
+            continue
+        else:
+            break
+    grades.append(_grade_in)
+    if 0 < _grade_in < 100:
+        grades.append(_grade_in)
+    else:
+        print("Invalid grade")
+        continue
+
+
