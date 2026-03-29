4a8d0ed596b25a5f4b05f2f6efda53c2a544fb6a
BoraAbramov <borya812@gmail.com>
2026-03-29 20:11:41 +0300

    3 half way 4 half way

--- answer4.py
+++ answer4.py
@@ -1,12 +1,27 @@
-
-def duplitector():
-    word = str(input("Enter a word: "))
+from logging import exception
 
 
+def str_list(words_list):
+    while True:
+        words_list = []
+        word = str(input("Enter a word: "))
+        if word == "quit":
+            return words_list
+        else:
+            words_list.append(word)
 
-
+def duplitector(words_list):
+    try:
+        for x in words_list:
+            x.lower(x.lower())
+            print(x, words_list)
+    except exception as e:
+        print(e)
 
 
 list1 = []
+
+str_list(list1)
+duplitector(list1)
 
 
