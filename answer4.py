00832579be7905f98414a99b568fe9ef9e80693b
BoraAbramov <borya812@gmail.com>
2026-03-29 20:29:57 +0300

    3 half way 4 maybe done

--- answer4.py
+++ answer4.py
@@ -1,22 +1,39 @@
-from logging import exception
 
 
 def str_list(words_list):
+    """
+
+    :param words_list: empty list or list of words
+    :return: add strings to list till 'quit'
+    """
+    words_list = []
     while True:
-        words_list = []
         word = str(input("Enter a word: "))
         if word == "quit":
             return words_list
         else:
             words_list.append(word)
 
+
 def duplitector(words_list):
-    try:
-        for x in words_list:
-            x.lower(x.lower())
-            print(x, words_list)
-    except exception as e:
-        print(e)
+    """
+
+    :param words_list: list of words
+    :return: check duplicates
+    """
+    sorter = dict()
+    for x in words_list:
+        sorter[x] = sorter.get(x, 0) + 1
+
+    for value in sorter.values():
+        if value > 1:
+            print("there were duplicates")
+            return
+
+    print("there were no duplicates")
+
+
+
 
 
 list1 = []
